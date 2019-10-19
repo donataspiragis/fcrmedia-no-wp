0e92324d5b5e3fdef6a9f0a8c0c25af80b041e16
Donatas Piragis <donatas.piragis@gmail.com>
2019-10-19 12:30:15 +0300

    First-structure-commit

---
+++ includes/html-head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="This is the most amazing landing page, which will rock your socks off.">
+    <meta name="keywords" content="Landing, design, style, one-page">
+    <meta name="author" content="Donatas Piragis">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Amazing landing page</title>
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include 'includes/html-head.php';
+include 'includes/header.php';
+include 'includes/landing-content.php';
+include 'includes/footer.php';
